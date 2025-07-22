343729e581317b31c76c1b4335eb77da7b42e81f
Yves Darbouze <y@massters.io>
2025-07-22 14:37:04 +0000

    The example games underneath the header should be in squares and be able

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
