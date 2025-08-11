3f138a9aebab6303ec29b487f81c6e2959021d9e
Yves Darbouze <y@massters.io>
2025-08-11 11:41:41 +0000

    I have a common hurdle with Firebase Authentication that is not working

---
+++ Users/yvesdarbouze/Playcer/src/lib/firebase.ts
@@ -0,0 +1,36 @@
+import { initializeApp, getApps, getApp, type FirebaseApp, type FirebaseOptions } from "firebase/app";
+import { getAuth, GoogleAuthProvider, TwitterAuthProvider, signOut as firebaseSignOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "playcer-xbv5e.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+
+// Initialize Firebase
+let app: FirebaseApp;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
+
+const signOut = () => {
+  return firebaseSignOut(auth);
+}
+
+// Export the initialized app instance
+export const getFirebaseApp = () => app;
+export { auth, firestore, storage, googleProvider, twitterProvider, signOut };
