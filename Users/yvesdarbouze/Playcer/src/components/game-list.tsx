b8e4ba212d26a9e503229439acbf915e636d3afb
Yves Darbouze <y@massters.io>
2025-08-21 13:16:35 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- Users/yvesdarbouze/Playcer/src/components/game-list.tsx
+++ Users/yvesdarbouze/Playcer/src/components/game-list.tsx
@@ -38,7 +38,7 @@ export function GameList({ initialGames }: GameListProps) {
                 <header className="relative flex items-center justify-center h-[60vh] md:h-[80vh] text-center overflow-hidden">
                     <div className="absolute top-0 left-0 w-full h-full">
                          <video
-                            src="https://firebasestorage.googleapis.com/v0/b/playcer-xbv5e.firebasestorage.app/o/Make_an_8_202508191324.mp?alt=media&token=435948f8-379c-4ddd-82e7-64051a84c405"
+                            src="https://firebasestorage.googleapis.com/v0/b/playcer-xbv5e.appspot.com/o/Make_an_8_202508191324.mp4?alt=media&token=435948f8-379c-4ddd-82e7-64051a84c405"
                             autoPlay
                             loop
                             muted
